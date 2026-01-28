0be4c080089c7c1de6a1d683b15a5cff32371eda
2012131967 <2012131967@qq.com>
2026-01-28 10:03:01 +0800

    7.4

---
+++ 1/7.4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+    int main(){
+
+        int a = 10;
+        int *p = NULL;
+
+        cout<< *p << endl;
+        cout << p << endl;
+
+        system("pause");
+        return 0;
+    }
